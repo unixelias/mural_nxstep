d6d87a342642094a48586695d48952f95c92c0e5
Elias Alves <unixelias@gmail.com>
2016-07-13 17:10:33 -0300

    Adicionado template de tela principal

--- js/menu.js
+++ js/menu.js
@@ -1,63 +1,65 @@
-// JavaScript Document
+﻿//JavaScript Document
 
-/*alert('a');
-confirm(); //return bool
+/*alert();
+confirm(); //retorna true ou false
 console.log();*/
 
+/*var x = document.getElementById('h1teste');
+x.innerHTML = "<span class='text-primary'>troca</span>";
 
-/*var x = document.getElementById('h1xulambs');
-x.innerHTML = "<span class='text-primary'>ciclambs</span>";
 console.log(x);*/
 
+
 $(document).ready(function(e) {
-    //var x = $('#h1xulambs');
-	//console.log(x);
-	
-	/* CADASTRO */
-	
-	$('#cadastro_treinador_link').click(function(e) {
-		e.preventDefault();
-    	//loader
-		$('#loader').load('viewers/cadastro/treinador.lista.php');
-    });
-	
-	$('#cadastro_treinamento_link').click(function(e) {
-		e.preventDefault();
-    	//loader
-		$('#loader').load('viewers/cadastro/treinamento.lista.php');
-    });
-	
-	$('#cadastro_trainee_link').click(function(e) {
-		e.preventDefault();
-    	//loader
-		$('#loader').load('viewers/cadastro/trainee.lista.php');
-    });
-	
-	$('#getout').click(function(e) {
-		e.preventDefault();
-        //loader
-		$.ajax({
-		   url: '../engine/controllers/logout.php',
-		   data: {
-				
-		   },
-		   error: function() {
-				alert('Erro na conexão com o servidor. Tente novamente em alguns segundos.');
-		   },
-		   success: function(data) {
-				console.log(data);
-				if(data === 'kickme'){
-					document.location.href = '../';
-				}
-				
-				
-				else{
-					alert('Erro ao conectar com banco de dados. Aguarde e tente novamente em alguns instantes.');	
-				}
-		   },
-		   
-		   type: 'POST'
-		});		
-    });
-	
+    
+		$(document).ready(function(e) {
+		
+			$('#cadastro_treinador_link').click(function(e) { //carregando parte de uma pagina na pagina principal
+				e.preventDefault();
+				//loader
+				$('#loader').load('viewers/cadastro/treinador.lista.php');
+			});
+			
+		});
+		
+		$(document).ready(function(e) {
+		
+			$('#cadastro_trainee_link').click(function(e) { //carregando parte de uma pagina na pagina principal
+				e.preventDefault();
+				//loader
+				$('#loader').load('viewers/cadastro/trainee.lista.php');
+			});
+			
+		});
+		
+		$(document).ready(function(e) {
+		
+			$('#cadastro_treinamento_link').click(function(e) { //carregando parte de uma pagina na pagina principal
+				e.preventDefault();
+				//loader
+				$('#loader').load('viewers/cadastro/treinamento.lista.php');
+			});
+			
+		});
+		
+			$(document).ready(function(e) {
+		
+			$('#gerenciamento_aula_link').click(function(e) { //carregando parte de uma pagina na pagina principal
+				e.preventDefault();
+				//loader
+				$('#loader').load('viewers/gerenciamento/aula.lista.php');
+			});
+			
+		});
+		
+			$(document).ready(function(e) {
+		
+			$('#gerenciamento_turma_link').click(function(e) { //carregando parte de uma pagina na pagina principal
+				e.preventDefault();
+				//loader
+				$('#loader').load('viewers/gerenciamento/turma.lista.php');
+			});
+			
+		});
+
 });
