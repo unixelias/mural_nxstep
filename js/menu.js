8cfb5434dea4cfa41c3a2151e6c69a9b94a7b18b
natancmacedo <natantur7@gmail.com>
2016-07-16 19:15:33 -0300

    Logout
    Logout funcional, testado.

--- js/menu.js
+++ js/menu.js
@@ -30,5 +30,34 @@ $(document).ready(function(e) {
 
 		});
 
+		$(document).ready(function(e) {
+
+			$('#getout').click(function(e) {
+				e.preventDefault();
+		        //loader
+				$.ajax({
+				   url: 'engine/controllers/logout.php',
+				   data: {
+
+				   },
+				   error: function() {
+						alert('Erro na conexão com o servidor. Tente novamente em alguns segundos.');
+				   },
+				   success: function(data) {
+						if(data === 'kickme'){
+							document.location.href = 'login/';
+						}
+
+
+						else{
+							alert('Erro ao conectar com banco de dados. Aguarde e tente novamente em alguns instantes.');
+						}
+				   },
+
+				   type: 'POST'
+					});
+		    });
+		});
+
 
 });
