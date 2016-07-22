c917b77c5b7083b7cf30bde053e4ff9b0aaac09b
Elias Alves <unixelias@gmail.com>
2016-07-22 19:44:52 -0300

    correções

--- viewers/mensagens/mensagem.nova.php
+++ viewers/mensagens/mensagem.nova.php
@@ -80,24 +80,11 @@
     <link rel="stylesheet" href="../../css/bootstrap.css">
 	<link rel="stylesheet" href="../../css/reset.css"> <!-- CSS reset -->
 	<link rel="stylesheet" href="../../css/style.css"> <!-- Resource style -->
-	<script src="js/modernizr.js"></script> <!-- Modernizr -->
+	
 
 <?php
 	require_once "../../engine/config.php";
 ?>
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href="../../css/bootstrap.css" rel="stylesheet" type="text/css">
-
-<link rel="stylesheet" href="../../css/reset.css"> <!-- CSS reset -->
-<link rel="stylesheet" href="../../css/style.css"> <!-- Resource style -->
-<script src="js/modernizr.js"></script> <!-- Modernizr -->
-<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-<!--[if lt IE 9]>
-<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->
 
 <ol class="breadcrumb">
   <li><a href="#">Home</a></li>
@@ -183,7 +170,7 @@
 </section> <!-- cd-timeline -->
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script src="js/main.js"></script> <!-- Resource jQuery -->
-<script src="js/modal.js"></script>
+<script src="js/modernizr.js"></script> <!-- Modernizr -->
 
 
 <br>
--- viewers/mensagens/mensagens.enviadas.lista.php
+++ viewers/mensagens/mensagens.enviadas.lista.php
@@ -6,21 +6,10 @@
 
     });
 </script>
-<script src="js/modernizr.js"></script> <!-- Modernizr -->
+
 <?php
 	require_once "../../engine/config.php";
 ?>
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-<!--[if lt IE 9]>
-<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->
-
-<!--Breadrumb-->
 <ol class="breadcrumb">
     <li><a href="#">Home</a></li>
     <li><a href="#">Mensagens</a></li>
@@ -81,3 +70,4 @@
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script src="js/main.js"></script>
 <script src="js/timeline.js"></script>
+<script src="js/modernizr.js"></script> <!-- Modernizr -->
